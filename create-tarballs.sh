98383d8ba707b8db26453e68e70f2ee3d3cd7fb1
GloriousEggroll <gloriouseggroll@gmail.com>
2026-03-21 14:25:33 -0600

    update gamescope session files for plasmalogin

--- create-tarballs.sh
+++ create-tarballs.sh
@@ -4,3 +4,4 @@ tar -cvzf gamescope-session-steam.tar.gz gamescope-session-steam
 tar -cvzf jupiter-hw-support.tar.gz jupiter-hw-support
 tar -cvzf kde-steamdeck.tar.gz kde-steamdeck
 tar -cvzf kde-steamdeck-additions.tar.gz kde-steamdeck-additions
+tar -cvzf gamescope-session-ogui.tar.gz gamescope-session-ogui
